dac8735c358b7533337f45e19b97e6fe44e5c63e
brGuirra <br.guirra@gmail.com>
2023-06-27 00:13:35 -0300

    feat: create task on form submition

--- src/components/molecules/form/form.tsx
+++ src/components/molecules/form/form.tsx
@@ -1,11 +1,32 @@
 import styles from "./form.module.css";
 
 import { Input, SubmitButton } from "components/atoms";
+import { useGlobalState } from "hooks/useGlobalState";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export const Form = (): JSX.Element => {
+  const { createTask } = useGlobalState();
+  const [newTask, setNewTask] = useState("");
+
+  function handleNewTaskDescription(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  async function handleNewTaskCreation(event: FormEvent) {
+    event.preventDefault();
+
+    await createTask(newTask);
+
+    setNewTask("");
+  }
+
   return (
-    <form className={styles.form}>
-      <Input placeholder="Adicione uma tarefa" />
+    <form className={styles.form} onSubmit={handleNewTaskCreation}>
+      <Input
+        placeholder="Adicione uma tarefa"
+        value={newTask}
+        onChange={handleNewTaskDescription}
+      />
       <SubmitButton />
     </form>
   );
